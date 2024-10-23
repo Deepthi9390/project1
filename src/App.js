a1979642eea4751c8cc6e22fd8b56cd928381346
Deepthi9390 <161426028+Deepthi9390@users.noreply.github.com>
2024-10-23 10:28:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+  const[name, setName] =useState("");
+  const[output, setOutput] =useState("");
+
+  function handleChange(e){
+    setName(e.target.value);
 }
+console.log(name)
 
-export default App;
+   function handleClick(){
+    // console.log(name)
+    setOutput(name)
+
+}
+   return(
+    
+    <div style={Styles.parent}>
+      <h1>Names</h1>
+      {/* <h3>{name}</h3> */}
+      <input type="text" style={Styles.inputstyles} onChange={handleChange}></input>
+      <button style={Styles.buttonstyles} onClick={handleClick}>Output</button>
+    </div>
+
+  )}
+  const Styles = {
+    parent:{
+      width:"100%", 
+      height: "100vh", 
+      display: "flex",
+      // background-color:""
+       alignItems: "center",
+        justifyContent: "center", 
+        flexDirection:"column",
+        gap:"20px"
+
+      },
+      inputstyles:{
+        width:"200px",
+        height:"25px", 
+        fontSize:"15px"
+      },
+      buttonstyles:{
+        width:"100px",
+        height:"40px",
+        marginTop:"30px"
+      }
+  }
